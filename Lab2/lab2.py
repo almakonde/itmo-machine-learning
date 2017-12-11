3a50b37e5b9a2e7056ff188c9487bdf095db34cf
Nikita Seleznev <nseleznev@biarum.com>
2017-12-11 20:21:50 +0300

    add pre-processing

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,16 +1,37 @@
+from typing import List, Set
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import wordpunct_tokenize
 
 
-def clean_text(text: str) -> str:
-    return text
+def tokenize_and_clean(text: str, stop: Set[str]) -> List[str]:
+    return [w for w in wordpunct_tokenize(text.lower()) if w.lower() not in stop]
 
 
 def get_dataset(filename: str):
+    nltk.download('stopwords')
+    stop = set(stopwords.words('english'))
+    stop.update([c for c in '.,"\'?!:;()[]{}'] + ['\x92'])
+
     data = []
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='iso-8859-1') as f:
         for line in f:
-            text, label = line.rsplit(',', 1)
+            if not line.strip():
+                continue
+
+            text, label = line.strip().rsplit(',', 1)
             if label not in {'spam', 'ham'}:
                 raise ValueError('Unknown label: ' + label)
+
+            text = tokenize_and_clean(text, stop)
             data.append((text, label,))
+    return data
+
+
+if __name__ == '__main__':
+    data = get_dataset('corpus/english.txt')
+    print(data)
+
 
 
